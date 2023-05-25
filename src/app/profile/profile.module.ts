8a3275117d0cbbaf50fbc4bfe1e1a3fd9b69f1a7
JoanArturo <joanarturo81@gmail.com>
2023-05-24 19:52:42 -0600

    Create About me section

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    AboutComponent,
   ],
 })
 export class ProfileModule { }
